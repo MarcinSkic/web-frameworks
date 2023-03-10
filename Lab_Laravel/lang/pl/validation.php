aad7915d9edaef100b7d2da6f55368617c4b3479
MarcinSkic <marcinskicoficjal@gmail.com>
2023-03-10 12:42:37 +0100

    Do lab1

---
+++ Lab_Laravel/lang/pl/validation.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'min' => [
+        'numeric' => 'Pole :attribute musi zawierać liczbę nie mniejszą niż :min.',
+        'file' => 'Plik :attribute musi zawierać minimum :min kilobajtów.',
+        'string' => 'Pole :attribute musi zawierać co najmniej :min znaków.',
+        'array' => 'Pole :attribute powinno zawierać minimum :min elementów.',
+        ],
+
+    'attributes' => [
+        'message' => 'komentarz',
+    ]
+    ];
---
+++ Lab_Laravel/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\CommentsController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/comments',[CommentsController::class,'index']);
+Route::get('/create',[CommentsController::class,'create'])->name('create');
+Route::post('/create',[CommentsController::class,'store'])->name('store');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
