ab6a8551c016ca4d53292e7a4f7f9ef4daf19432
Marcin Skic <marcinskicoficjal@gmail.com>
2023-03-18 20:04:12 +0100

    Fix formatting

--- Lab2_AdvJS/ex3_arrayFunctionsChaining.js
+++ Lab2_AdvJS/ex3_arrayFunctionsChaining.js
@@ -25,23 +25,21 @@ const wtorek = [
 ]
 
 console.log(
-poniedzialek.reduce((acc,el) => {
-    acc.push(el)
-    return acc
-},    //This line is stupid, I know
-wtorek)
-.map(el => {
-    el.czas /= 60
-    return el
-})
-.filter(el => el.czas <= 2)
-.map(el => {
-    el.czas *= 35
-    return el
-})
-.reduce((acc,el) => [acc[0] + el.czas]
-,[0])
-.map(el => `${el}.00PLN`)
-.reduce((_,el) => el
-,'')
+    poniedzialek
+        .reduce((acc, el) => {
+            acc.push(el)
+            return acc
+        }, wtorek)
+        .map(el => {
+            el.czas /= 60
+            return el
+        })
+        .filter(el => el.czas <= 2)
+        .map(el => {
+            el.czas *= 35
+            return el
+        })
+        .reduce((acc, el) => [(+acc) + el.czas], [0])
+        .map(el => `${el}.00PLN`)
+        .reduce((_, el) => el, '')
 )
